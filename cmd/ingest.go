e5b99d28720a7cd8ad27e6ead666c602594cb64a
Markus Opolka <markus.opolka@netways.de>
2023-06-13 16:50:09 +0200

    Add sub-check for Ingest Pipelines
     - checks for failed operations on pipelines

---
+++ cmd/ingest.go
@@ -0,0 +1,131 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/NETWAYS/go-check"
+	"github.com/NETWAYS/go-check/perfdata"
+	"github.com/NETWAYS/go-check/result"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+// To store the CLI parameters
+type PipelineConfig struct {
+	PipelineName   string
+	FailedWarning  string
+	FailedCritical string
+}
+
+var cliPipelineConfig PipelineConfig
+
+var ingestCmd = &cobra.Command{
+	Use:   "ingest",
+	Short: "Checks the ingest statistics of Ingest Pipelines",
+	Long:  `Checks the ingest statistics of Ingest Pipelines`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var (
+			rc       int
+			output   string
+			perfList perfdata.PerfdataList
+		)
+
+		failedCrit, err := check.ParseThreshold(cliPipelineConfig.FailedCritical)
+		if err != nil {
+			check.ExitError(err)
+		}
+
+		failedWarn, err := check.ParseThreshold(cliPipelineConfig.FailedWarning)
+		if err != nil {
+			check.ExitError(err)
+		}
+
+		client := cliConfig.NewClient()
+
+		stats, err := client.NodeStats()
+		if err != nil {
+			check.ExitError(err)
+		}
+
+		// Calculate states capacity
+		cap := 0
+		for _, node := range stats.Nodes {
+			cap += len(node.Ingest.Pipelines)
+		}
+
+		states := make([]int, 0, cap)
+
+		// Check status for each pipeline
+		var summary strings.Builder
+
+		for _, node := range stats.Nodes {
+			for pipelineName, pp := range node.Ingest.Pipelines {
+
+				// Skip if pipeline name doesn't match
+				if cliPipelineConfig.PipelineName != "" {
+					if cliPipelineConfig.PipelineName != pipelineName {
+						continue
+					}
+				}
+
+				summary.WriteString("\n \\_")
+				if failedCrit.DoesViolate(pp.Failed) {
+					states = append(states, check.Critical)
+					summary.WriteString(fmt.Sprintf("[CRITICAL] Failed ingest operations for %s: %g;", pipelineName, pp.Failed))
+				} else if failedWarn.DoesViolate(pp.Failed) {
+					states = append(states, check.Warning)
+					summary.WriteString(fmt.Sprintf("[WARNING] Failed ingest operations for %s: %g;", pipelineName, pp.Failed))
+				} else {
+					states = append(states, check.OK)
+					summary.WriteString(fmt.Sprintf("[OK] Failed ingest operations for %s: %g;", pipelineName, pp.Failed))
+				}
+
+				perfList.Add(&perfdata.Perfdata{
+					Label: fmt.Sprintf("pipelines.%s.failed", pipelineName),
+					Uom:   "c",
+					Value: pp.Failed})
+				perfList.Add(&perfdata.Perfdata{
+					Label: fmt.Sprintf("pipelines.%s.count", pipelineName),
+					Uom:   "c",
+					Value: pp.Count})
+				perfList.Add(&perfdata.Perfdata{
+					Label: fmt.Sprintf("pipelines.%s.current", pipelineName),
+					Uom:   "c",
+					Value: pp.Current})
+			}
+		}
+
+		// Validate the various subchecks and use the worst state as return code
+		switch result.WorstState(states...) {
+		case 0:
+			rc = check.OK
+			output = "Ingest operations alright"
+		case 1:
+			rc = check.Warning
+			output = "Ingest operations may not be alright"
+		case 2:
+			rc = check.Critical
+			output = "Ingest operations not alright"
+		default:
+			rc = check.Unknown
+			output = "Ingest operations status unknown"
+		}
+
+		check.ExitRaw(rc, output, summary.String(), "|", perfList.String())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(ingestCmd)
+
+	fs := ingestCmd.Flags()
+
+	fs.StringVar(&cliPipelineConfig.PipelineName, "pipeline", "",
+		"Pipeline Name")
+
+	fs.StringVar(&cliPipelineConfig.FailedWarning, "failed-warning", "10",
+		"Warning threshold for failed ingest operations. Use min:max for a range.")
+	fs.StringVar(&cliPipelineConfig.FailedCritical, "failed-critical", "20",
+		"Critical threshold for failed ingest operations. Use min:max for a range.")
+
+	fs.SortFlags = false
+}
---
+++ cmd/ingest_test.go
@@ -0,0 +1,98 @@
+package cmd
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestIngest_ConnectionRefused(t *testing.T) {
+
+	cmd := exec.Command("go", "run", "../main.go", "ingest", "--port", "9999")
+	out, _ := cmd.CombinedOutput()
+
+	actual := string(out)
+	expected := "UNKNOWN - could not fetch cluster nodes statistics: Get \"http://localhost:9999/_nodes/stats\": dial"
+
+	if !strings.Contains(actual, expected) {
+		t.Error("\nActual: ", actual, "\nExpected: ", expected)
+	}
+}
+
+type IngestTest struct {
+	name     string
+	server   *httptest.Server
+	args     []string
+	expected string
+}
+
+func TestIngestCmd(t *testing.T) {
+	tests := []IngestTest{
+		{
+			name: "ingest-ok",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"_nodes":{"total":1,"successful":1,"failed":0},"cluster_name":"clustername","nodes":{"node1":{"ip":"127.0.0.1:9300","ingest":{"total":{"count":10,"time_in_millis":0,"current":3,"failed":5},"pipelines":{"mypipeline":{"count":10,"time_in_millis":0,"current":3,"failed":5,"processors":[{"set":{"type":"set","stats":{"count":0,"time_in_millis":0,"current":0,"failed":0}}}]}}}}}}`))
+			})),
+			args:     []string{"run", "../main.go", "ingest"},
+			expected: "OK - Ingest operations alright \n \\_[OK] Failed ingest operations for mypipeline: 5; | pipelines.mypipeline.failed=5c pipelines.mypipeline.count=10c pipelines.mypipeline.current=3c\n",
+		},
+		{
+			name: "ingest-ok-with-name",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"_nodes":{"total":1,"successful":1,"failed":0},"cluster_name":"clustername","nodes":{"mm3U-u0WTCeuZ325vZGY2w":{"ip":"127.0.0.1:9300","ingest":{"total":{"count":10,"time_in_millis":0,"current":3,"failed":5},"pipelines":{"foobar":{"count":10,"time_in_millis":0,"current":3,"failed":5,"processors":[{"set":{"type":"set","stats":{"count":0,"time_in_millis":0,"current":0,"failed":0}}}]},"mypipeline":{"count":10,"time_in_millis":0,"current":3,"failed":5,"processors":[{"set":{"type":"set","stats":{"count":0,"time_in_millis":0,"current":0,"failed":0}}}]}}}}}}`))
+			})),
+			args:     []string{"run", "../main.go", "ingest", "--pipeline", "foobar"},
+			expected: "OK - Ingest operations alright \n \\_[OK] Failed ingest operations for foobar: 5; | pipelines.foobar.failed=5c pipelines.foobar.count=10c pipelines.foobar.current=3c\n",
+		},
+		{
+			name: "ingest-warn",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"_nodes":{"total":1,"successful":1,"failed":0},"cluster_name":"clustername","nodes":{"node1":{"ip":"127.0.0.1:9300","ingest":{"total":{"count":10,"time_in_millis":0,"current":3,"failed":5},"pipelines":{"mypipeline":{"count":10,"time_in_millis":0,"current":3,"failed":5,"processors":[{"set":{"type":"set","stats":{"count":0,"time_in_millis":0,"current":0,"failed":0}}}]}}}}}}`))
+			})),
+			args:     []string{"run", "../main.go", "ingest", "--failed-warning", "3"},
+			expected: "WARNING - Ingest operations may not be alright \n \\_[WARNING] Failed ingest operations for mypipeline: 5; | pipelines.mypipeline.failed=5c pipelines.mypipeline.count=10c pipelines.mypipeline.current=3c\nexit status 1\n",
+		},
+		{
+			name: "ingest-crit",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"_nodes":{"total":1,"successful":1,"failed":0},"cluster_name":"clustername","nodes":{"node1":{"ip":"127.0.0.1:9300","ingest":{"total":{"count":10,"time_in_millis":0,"current":3,"failed":5},"pipelines":{"mypipeline":{"count":10,"time_in_millis":0,"current":3,"failed":5,"processors":[{"set":{"type":"set","stats":{"count":0,"time_in_millis":0,"current":0,"failed":0}}}]}}}}}}`))
+			})),
+			args:     []string{"run", "../main.go", "ingest", "--failed-critical", "3"},
+			expected: "CRITICAL - Ingest operations not alright \n \\_[CRITICAL] Failed ingest operations for mypipeline: 5; | pipelines.mypipeline.failed=5c pipelines.mypipeline.count=10c pipelines.mypipeline.current=3c\nexit status 2\n",
+		},
+		{
+			name: "ingest-invalid",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{}`))
+			})),
+			args:     []string{"run", "../main.go", "ingest"},
+			expected: "UNKNOWN - Ingest operations status unknown  | \nexit status 3\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			defer test.server.Close()
+
+			// We need the random Port extracted
+			u, _ := url.Parse(test.server.URL)
+			cmd := exec.Command("go", append(test.args, "--port", u.Port())...)
+			out, _ := cmd.CombinedOutput()
+
+			actual := string(out)
+
+			if actual != test.expected {
+				t.Error("\nActual: ", actual, "\nExpected: ", test.expected)
+			}
+
+		})
+	}
+}
